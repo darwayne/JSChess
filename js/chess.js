a7839be543903f3cc729bbc7e2817a846529fca0
Darwayne <darwaynelynch@gmail.com>
2015-04-23 19:33:16 -0400

    Added check detection and updated king to follow real rules.

--- js/chess.js
+++ js/chess.js
@@ -17,10 +17,26 @@ function permutatedValidMoves(keep_checking){
     return this.getValidMovesByPermutations(permutations, keep_checking, position, my_color, board);
   };
 }
+function getDistance(a, b){
+  return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+function getDistanceBetweenPositions(pos1, pos2){
+  var a = positionToCoordinate(pos1);
+  var b = positionToCoordinate(pos2);
+  return getDistance(a, b);
+}
+
+function positionToCoordinate(pos){
+  var position = pos.toUpperCase();
+  var x = position.charCodeAt(0) - 65;
+  var y = parseInt(position[1], 10);
+  return {x: x, y: y}
+}
 
 function ChessBoard(){
   var colors = ['white', 'black'];
   _.extend(this, {
+    colors: colors,
     whitePiecesEaten: [],
     blackPiecesEaten: [],
     turn: colors[0],
@@ -80,8 +96,10 @@ _.extend(ChessBoard.prototype, {
         color = parseInt(pos[1], 10) > 5 ? 'black' : 'white';
         var piece = new window[piece_name](color, pos, board);
         board[color + 'Pieces'].push(piece);
+        //console.log(piece.name, piece.color);
         if(piece.name == 'king'){
-          self[color + 'King'] = piece;
+          board[color + 'King'] = piece;
+          //console.log('%s king set', piece.color);
         }
         board.setPos(pos, piece);
       }
@@ -131,6 +149,35 @@ function ChessPiece(color, pos, board)
 }
 
 _.extend(ChessPiece.prototype, {
+  getVirtualBoard: function(){
+      var vb = angular.copy(this.board);
+      vb.virtual = true;
+
+    return vb;
+  },
+  myKingChecked: function(){
+    return this.board[this.color + 'InCheck'];
+  },
+  detectCheck: function(move, would_stop){
+    var virtual_board     = this.getVirtualBoard();
+    var my_pos            = this.pos;
+    var virtual_me        = virtual_board.getPos(my_pos);
+    var would_cause       = !would_stop;
+    var currently_checked = this.myKingChecked();
+    if(virtual_me.canMoveTo(move)){
+      virtual_me.moveToPos(move);
+      var virtual_check = virtual_me.myKingChecked();
+      if(would_stop){
+        if(!virtual_check){
+          return true;
+        }
+      } else if(would_cause){
+        if(virtual_check){
+          return true;
+        }
+      }
+    }
+  },
   getEnemyColor: function(color){
     var my_color = color || this.color;
     var result = my_color == 'white' ? 'black' : 'white';
@@ -139,11 +186,15 @@ _.extend(ChessPiece.prototype, {
   isCheckingEnemyKing: function(){
     var enemy_color   = this.getEnemyColor();
     var enemy_king    = this.board[enemy_color + 'King'];
-    var checking_king = this.getValidMoves().indexOf(enemy_king.pos);
-    this.board[enemy_color + 'InCheck'] = checking_king;
+    var checking_king = this.getValidMoves().indexOf(enemy_king.pos) >= 0;
+    
+    if(checking_king){
+      this.board[enemy_color + 'InCheck'] = checking_king;
+    }
     return checking_king;
   },
   moveToPos: function(pos){
+    var self = this;
     this.moves++;
     var enemy_color = this.getEnemyColor();
     if(this.enemyAtPos(pos)){
@@ -154,19 +205,35 @@ _.extend(ChessPiece.prototype, {
       }
       var enemy_pieces = this.board[enemy_color + 'Pieces'];
       var index = enemy_pieces.indexOf(enemy);
+      enemy.captured = true;
       if(index >= 0){
         enemy_pieces.splice(index, 1);
       }
     }
-    _.each(this.board.colors, function(color){
-      _.each(this.board[color + 'Pieces'], function(piece){
-        piece.isCheckingEnemyKing();
-      });
-    });
     this.board.setPos(pos, this);
     this.board.setPos(this.pos, '');
     this.pos = pos;
 
+    _.each([self.color, enemy_color], function(color){
+      var pieces = self.board[color + 'Pieces'];
+      var e_color = pieces[0].getEnemyColor();
+      var not_checked = true;
+      _.each(pieces, function(piece){
+        if(piece.isCheckingEnemyKing()){
+          console.log(piece.color, piece.name, 'is checking enemy');
+          not_checked = false;
+        }
+      });
+      var enemy_in_check = self.board[e_color + 'InCheck'];
+      // if(self.name == 'queen' && pos.toUpperCase() == 'E2'){
+      //   debugger;
+      // }
+      if(not_checked && enemy_in_check){
+        self.board[e_color + 'InCheck'] = false;
+      }
+    });
+
+    this.board.turn = this.board.turn == 'white' ? 'black' : 'white';
     return this.moves;
   },
   moveTo: function(pos){
@@ -186,31 +253,28 @@ _.extend(ChessPiece.prototype, {
     var enemy_color   = this.getEnemyColor();
     var self          = this;
     var is_valid_move = this.canMoveTo(pos);
-    if(is_valid_move && this.board[this.color + 'InCheck']){
-      if(this.wouldMoveStopCheck(pos)){
-        return true;
+    var i_am_in_check = this.myKingChecked();
+    if(is_valid_move){
+      if(i_am_in_check){
+        return this.wouldMoveStopCheck(pos);
       }
-      else
-      {
-        return false;
+      else{
+        return !this.wouldMoveCauseCheck(pos);
       }
     }
 
     return is_valid_move;
   },
   wouldMoveStopCheck: function(move){
-    var virtual_board = _.cloneDeep(this.board);
-    var my_pos = this.pos;
-    var virtual_me = virtual_board.getPos(my_pos);
-    if(virtual_me.canMoveTo(move)){
-      virtual_me.moveToPos(pos);
-      if(!virtual_board[this.color + 'InCheck']){
-        return true;
-      }
-    }
+    return this.detectCheck(move, true);
+  },
+  wouldMoveCauseCheck: function(move){
+    return this.detectCheck(move);
   },
   canMoveTo: function(pos){
-    return (valid_moves.indexOf(pos.toUpperCase()) >= 0);
+    return (this.getValidMoves().map(function(move){
+      return move.toUpperCase();
+    }).indexOf(pos.toUpperCase()) >= 0);
   },
   posMath: function(arg1, arg2){
     var pos = arg1;
@@ -270,8 +334,9 @@ _.extend(Knight.prototype, ChessPiece.prototype, {
     var valid_moves = [];
     var result;
     for(var i = 0; i < possible_moves.length; i++){
+
       if(this.clearToMove(possible_moves[i])){
-        valid_moves.push(possible_moves);
+        valid_moves.push(possible_moves[i]);
       }
     }
 
@@ -345,7 +410,12 @@ function King(color, pos, board){
 
 _.extend(King.prototype, ChessPiece.prototype, {
   permutations: Queen.prototype.permutations,
-  getValidMoves: permutatedValidMoves(false)
+  getValidMoves: function(){
+    var enemy_king_pos = this.board[this.getEnemyColor() + 'King'].pos;
+    return _.reject(permutatedValidMoves(false).apply(this, arguments), function(pos){
+      return getDistanceBetweenPositions(pos, enemy_king_pos) < 2;
+    });
+  }
 });
 
 function Pawn(color, pos, board){
@@ -384,4 +454,26 @@ _.extend(Pawn.prototype, ChessPiece.prototype, {
 });
 
 
+// board = new ChessBoard();
+// board.getPos('e2').moveTo('e4');
+// board.getPos('d7').moveTo('d5');
+// board.getPos('e4').moveTo('d5');
+// board.getPos('d8').moveTo('d5');
+// board.getPos('a2').moveTo('a3');
+// board.getPos('d5').moveTo('e4'); //check
+// board.getPos('d1').moveTo('e2'); //should stop check
+
+board = new ChessBoard();
+moves('e2, e3, e7, e6, e1, e2, e8, e7, e2, d3, e7, d6, d3, d4');
+
+function move(from, to){
+  board.getPos(from).moveTo(to);
+  return move;
+}
+function moves(str){
+  var arr = str.split(', ');
+  for(var i = 0; i < arr.length - 1; i+=2){
+    move(arr[i], arr[i+1]);
+  }
+}
 
